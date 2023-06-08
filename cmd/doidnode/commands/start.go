1062510f37e69f95af662425620c71c0956c8ed2
pipeline78 <pipeline78@gmail.com>
2023-06-08 18:55:42 +0800

    libp2p

--- cmd/doidnode/commands/start.go
+++ cmd/doidnode/commands/start.go
@@ -3,37 +3,37 @@ package commands
 import (
 	"fmt"
 
-	"github.com/DOIDFoundation/node/node"
+	"github.com/DOIDFoundation/node/network"
 	"github.com/cometbft/cometbft/libs/os"
 	"github.com/spf13/cobra"
 )
 
-// addFlags exposes configuration options for starting a node.
+// addFlags exposes configuration options for starting a network.
 func addFlags(cmd *cobra.Command) {
 }
 
-// StartCmd is the command that allows the CLI to start a node.
+// StartCmd is the command that allows the CLI to start a network.
 var StartCmd = &cobra.Command{
 	Use:     "start",
-	Aliases: []string{"node", "run"},
-	Short:   "Run the CometBFT node",
+	Aliases: []string{"network", "run"},
+	Short:   "Run the CometBFT network",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		n, err := node.NewNode(logger)
+		n, err := network.NewNode(logger)
 		if err != nil {
-			return fmt.Errorf("failed to create node: %w", err)
+			return fmt.Errorf("failed to create network: %w", err)
 		}
 
 		if err := n.Start(); err != nil {
-			return fmt.Errorf("failed to start node: %w", err)
+			return fmt.Errorf("failed to start network: %w", err)
 		}
 
-		logger.Info("started node")
+		logger.Info("started network")
 
 		// Stop upon receiving SIGTERM or CTRL-C.
 		os.TrapSignal(logger, func() {
 			if n.IsRunning() {
 				if err := n.Stop(); err != nil {
-					logger.Error("unable to stop the node", "error", err)
+					logger.Error("unable to stop the network", "error", err)
 				}
 			}
 		})
--- node/config.go
+++
@@ -1,8 +0,0 @@
-package node
-
-// Config defines the top level configuration for a node
-type Config struct {
-}
-
-// DefaultConfig returns a default configuration for a  node
-var DefaultConfig = Config{}
