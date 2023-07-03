0a2cc553a38efaed5f33e949f8e0996a3b7d69d6
ac.doid <116242667+ac-doid@users.noreply.github.com>
2023-07-03 13:57:46 +0800

    allow use current working dir as home

--- cmd/doidnode/main.go
+++ cmd/doidnode/main.go
@@ -1,18 +1,11 @@
 package main
 
 import (
-	"os"
-	"path/filepath"
-
 	"github.com/DOIDFoundation/node/cmd/doidnode/commands"
-
-	"github.com/cometbft/cometbft/libs/cli"
 )
 
 func main() {
-	cmd := cli.PrepareBaseCmd(commands.RootCmd, "DOID", os.ExpandEnv(filepath.Join("$HOME", ".doidnode")))
-
-	if err := cmd.Execute(); err != nil {
+	if err := commands.RootCmdExecutor().Execute(); err != nil {
 		panic(err)
 	}
 }
