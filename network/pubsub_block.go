7ba044230f22cca3e0c96dc3c9689e1d2cf6d233
ac.doid <116242667+ac-doid@users.noreply.github.com>
2023-07-31 17:22:20 +0800

    improve fork detection, only start sync when td is high

--- network/pubsub_block.go
+++ network/pubsub_block.go
@@ -1,18 +1,10 @@
 package network
 
 import (
-	"math/big"
-
 	"github.com/DOIDFoundation/node/events"
-	"github.com/DOIDFoundation/node/types"
 	"github.com/ethereum/go-ethereum/rlp"
 	pubsub "github.com/libp2p/go-libp2p-pubsub"
 )
-
-type newBlock struct {
-	Block *types.Block
-	Td    *big.Int
-}
 
 func (n *Network) registerBlockSubscribers() {
 	logger := n.Logger.With("topic", TopicBlock)
@@ -27,8 +19,8 @@ func (n *Network) registerBlockSubscribers() {
 	// message.
 	pipeline := func(msg *pubsub.Message) {
 		data := msg.GetData()
-		blockEvent := new(newBlock)
-		err := rlp.DecodeBytes(data, blockEvent)
+		var blockEvent events.BlockWithTd
+		err := rlp.DecodeBytes(data, &blockEvent)
 		if err != nil {
 			logger.Error("failed to decode received block", "err", err)
 			return
@@ -36,7 +28,7 @@ func (n *Network) registerBlockSubscribers() {
 		logger.Debug("got block", "height", blockEvent.Block.Header.Height, "td", blockEvent.Td, "peer", msg.GetFrom())
 		updatePeerState(n.host.Peerstore(), msg.GetFrom(), &state{Height: blockEvent.Block.Header.Height.Uint64(), Td: blockEvent.Td})
 
-		events.NewNetworkBlock.Send(blockEvent.Block)
+		events.NewNetworkBlock.Send(blockEvent)
 	}
 
 	go pubsubMessageLoop(ctx, topic, n.host.ID(), pipeline, logger)
